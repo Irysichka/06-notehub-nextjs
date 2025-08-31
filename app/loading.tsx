6ecf4d7e56b03babb90c695429fa6d6d988b8216
Irina <Irysichka2704@gmail.com>
2025-08-31 20:22:13 +0300

    add

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,4 @@
-import css from "@/app/notes/loading.module.css"
+import css from "@/app/loading.module.css"
 
 export default function Loading() {
     return <p className={css.wrapper}>Loading, please wait...</p>
